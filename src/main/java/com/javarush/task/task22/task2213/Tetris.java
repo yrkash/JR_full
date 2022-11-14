f28068df33b6e6ff5334671ef8eaad4793bd1286
yrkash <yrkash@gmail.com>
2022-11-14 16:58:19 +0300

    221114 Json

--- src/main/java/com/javarush/task/task22/task2213/Tetris.java
+++ src/main/java/com/javarush/task/task22/task2213/Tetris.java
@@ -11,24 +11,7 @@ public class Tetris {
     private Figure figure;
 
     public static void main(String[] args) throws IOException {
-        //создание объекта для сериализации в JSON
-        Cat cat = new Cat();
-        cat.name = "Murka";
-        cat.age = 5;
-        cat.weight = 4;
 
-        //писать результат сериализации будем во Writer(StringWriter)
-        StringWriter writer = new StringWriter();
-
-        //это объект Jackson, который выполняет сериализацию
-        ObjectMapper mapper = new ObjectMapper();
-
-        // сама сериализация: 1-куда, 2-что
-        mapper.writeValue(writer, cat);
-
-        //преобразовываем все записанное во StringWriter в строку
-        String result = writer.toString();
-        System.out.println(result);
 
     }
 }
