0fc6dea0c341364dcd858bee667d0869ac2f1799
yrkash <yrkash@gmail.com>
2022-11-17 22:18:06 +0300

    221117 Tetris (2213)

--- src/main/java/com/javarush/task/task22/task2213/Tetris.java
+++ src/main/java/com/javarush/task/task22/task2213/Tetris.java
@@ -1,17 +1,27 @@
 package com.javarush.task.task22.task2213;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.io.StringWriter;
 
 public class Tetris {
 
     private Field field;
     private Figure figure;
+    public static Tetris game;
+    public Field getField() {
+        return field;
+    }
 
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void run() {}
+
+    public void step() {}
     public static void main(String[] args) throws IOException {
-
+        Tetris.game = new Tetris();
+        game.run();
 
     }
 }
