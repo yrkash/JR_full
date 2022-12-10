9ab39eb55ba655cea80a35a4e2c498f0fb83ef82
yrkash <yrkash@gmail.com>
2022-12-10 17:13:06 +0300

    Shortener. @Test

---
+++ src/test/java/com/javarush/task/task33/task3310/FunctionalTest.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.Shortener;
+import com.javarush.task.task33.task3310.strategy.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FunctionalTest {
+
+    public void testStorage(Shortener shortener) {
+        String str1 = new String("Test string 1");
+        String str2 = new String("Test string 2");
+        String str3 = new String("Test string 1");
+
+        Long id1 = shortener.getId(str1);
+        Long id2 = shortener.getId(str2);
+        Long id3 = shortener.getId(str3);
+
+        Assert.assertNotEquals(id1, id2);
+        Assert.assertNotEquals(id3, id2);
+        Assert.assertEquals(id1, id3);
+
+        String actualStr1 = shortener.getString(id1);
+        String actualStr2 = shortener.getString(id2);
+        String actualStr3 = shortener.getString(id3);
+
+        Assert.assertEquals(str1, actualStr1);
+        Assert.assertEquals(str2, actualStr2);
+        Assert.assertEquals(str3, actualStr3);
+    }
+    @Test
+    public void testHashMapStorageStrategy() {
+        HashMapStorageStrategy storageStrategy = new HashMapStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+    @Test
+    public void testOurHashMapStorageStrategy() {
+        OurHashMapStorageStrategy storageStrategy = new OurHashMapStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+    @Test
+    public void testFileStorageStrategy() {
+        FileStorageStrategy storageStrategy = new FileStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+    @Test
+    public void testHashBiMapStorageStrategy() {
+        HashBiMapStorageStrategy storageStrategy = new HashBiMapStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+    @Test
+    public void testDualHashBidiMapStorageStrategy() {
+        DualHashBidiMapStorageStrategy storageStrategy = new DualHashBidiMapStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+    @Test
+    public void testOurHashBiMapStorageStrategy() {
+        OurHashBiMapStorageStrategy storageStrategy = new OurHashBiMapStorageStrategy();
+        Shortener shortener = new Shortener(storageStrategy);
+        testStorage(shortener);
+    }
+}
---
+++ src/test/java/com/javarush/task/task33/task3310/SpeedTest.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.HashBiMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SpeedTest {
+
+
+    public long getTimeToGetIds(Shortener shortener, Set<String> strings, Set<Long> ids) {
+        Date startTimestamp = new Date();
+        for (String s : strings)
+            ids.add(shortener.getId(s));
+        Date endTimestamp = new Date();
+        return endTimestamp.getTime() - startTimestamp.getTime();
+    }
+
+    public long getTimeToGetStrings(Shortener shortener, Set<Long> ids, Set<String> strings) {
+        Date startTimestamp = new Date();
+        for (Long id : ids)
+            strings.add(shortener.getString(id));
+        Date endTimestamp = new Date();
+        return endTimestamp.getTime() - startTimestamp.getTime();
+    }
+
+    @Test
+    public void testHashMapStorage() {
+        Shortener shortener1 = new Shortener(new HashMapStorageStrategy());
+        Shortener shortener2 = new Shortener(new HashBiMapStorageStrategy());
+        Set<String> origStrings = new HashSet<>();
+        for (int i = 0; i < 10000; i++) {
+            origStrings.add(Helper.generateRandomString());
+        }
+        Set<Long> ids1 = new HashSet<>();
+        Set<Long> ids2 = new HashSet<>();
+        Long idToStringTime1  = getTimeToGetIds(shortener1, origStrings, ids1);
+        Long idToStringTime2  = getTimeToGetIds(shortener2, origStrings, ids2);
+        Assert.assertTrue(idToStringTime1  > idToStringTime2);
+
+        Set<String> strings1 = new HashSet<>();
+        Set<String> strings2 = new HashSet<>();
+        Long IdToStringTime1 = getTimeToGetStrings(shortener1, ids1, strings1);
+        Long IdToStringTime2 = getTimeToGetStrings(shortener1, ids2, strings2);
+        Assert.assertEquals(IdToStringTime1, IdToStringTime2, 30f);
+    }
+}
