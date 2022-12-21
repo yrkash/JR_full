25b58c9c4b6dfe720becb5083bee908b14b11844
yrkash <yrkash@gmail.com>
2022-12-21 17:05:06 +0300

    Lvl38.Annotation+

---
+++ src/main/java/com/javarush/task/task38/task3812/PrepareMyTest.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task38.task3812;
+
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PrepareMyTest {
+    Class<?>[] value() default Solution.class;
+
+    String[] fullyQualifiedNames() default "";
+}
